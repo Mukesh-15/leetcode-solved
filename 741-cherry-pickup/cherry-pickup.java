7e808c0c4b0a86c477b541dc9b812879f0a359bc
Mukesh-15 <mukeshdoodapaka@gmail.com>
2026-08-13 00:34:28 +0530

    Time: 13 ms (97.45%) | Memory: 46.8 MB (93.50%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,38 +1,41 @@
 class Solution {
-    int[][] moves = {{-1, 0}, {0, -1}};
-    Integer[][][][] dp;
+    Integer[][][] dp;
 
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
-        dp = new Integer[n][n][n][n];
+        dp = new Integer[n][n][n];
 
-        return Math.max(0, helper(grid, n - 1, n - 1, n - 1, n - 1));
+        return Math.max(0, helper(grid, n - 1, n - 1, n - 1));
     }
 
-    private int helper(int[][] grid, int i1, int j1, int i2,  int j2){
+    private int helper(int[][] grid, int i1, int j1, int i2) {
+        int j2 = i1 + j1 - i2;
+
         if(i1 < 0 || i2 < 0 || j1 < 0 || j2 < 0) return -1;
-        if(i1 == 0 && j1 == 0 && i2 == 0 && j2 == 0){
-            return grid[0][0];
-        }
 
         if(grid[i1][j1] == -1 || grid[i2][j2] == -1){
             return -1;
         }
 
-        if(dp[i1][j1][i2][j2] != null) return dp[i1][j1][i2][j2];
+        if(i1 == 0 && j1 == 0){
+            return grid[0][0];
+        }
+
+        if(dp[i1][j1][i2] != null){
+            return dp[i1][j1][i2];
+        }
 
         int curr = (i1 == i2 && j1 == j2) ? grid[i1][j1] : grid[i1][j1] + grid[i2][j2];
 
         int max = -1;
-        
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                max = Math.max(max, helper(grid, i1 + moves[i][0], j1 + moves[i][1], i2 + moves[j][0], j2 + moves[j][1]));
-            }
-        }
 
-        dp[i1][j1][i2][j2] = (max == -1) ? max : curr + max;
+        max = Math.max(max, helper(grid, i1 - 1, j1, i2 - 1));
+        max = Math.max(max, helper(grid, i1 - 1, j1, i2));
+        max = Math.max(max, helper(grid, i1, j1 - 1, i2 - 1));
+        max = Math.max(max, helper(grid, i1, j1 - 1, i2));
 
-        return dp[i1][j1][i2][j2];
+        dp[i1][j1][i2] = (max == -1) ? -1 : curr + max;
+
+        return dp[i1][j1][i2];
     }
 }
